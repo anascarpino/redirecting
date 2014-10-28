4867df97d2534e02bb00f16669d9c4f6fed7e72a
Ana Beatriz <anascarpino@gmail.com>
2014-10-28 11:55:38 -0200

    testando

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+function getMobileOperatingSystem() {
+	if(navigator.userAgent.match(/Android/i)){
+		return "https://play.google.com/";
+	}else if(navigator.userAgent.match(/iPhone/i)){
+		return "https://itunes.apple.com/br/genre/ios/id36?mt=8";
+	}else if(navigator.userAgent.match(/iPod/i)){
+		return "https://itunes.apple.com/br/genre/ios/id36?mt=8";
+	}else if(navigator.userAgent.match(/iPad/i)){
+		return "https://itunes.apple.com/br/genre/ios/id36?mt=8";
+	}else{
+		return "https://google.com/";
+	}
+}
+		
